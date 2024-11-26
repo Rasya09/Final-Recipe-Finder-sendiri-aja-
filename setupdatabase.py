d2d098935311cfe78c3d2075f255a3f5c6b6cd51
Rasya09 <rasyafadil09@gmail.com>
2024-11-26 22:28:52 +0700

    membuat fungsi menambahkan database dengan melalui di cek terlebih dahulu

--- setupdatabase.py
+++ setupdatabase.py
@@ -11,7 +11,28 @@ def connect_to_database():
 # Output: Mengembalikan objek koneksi ke database.
 # Tujuan: Membuat koneksi ke server MySQL
 
+# Fungsi untuk membuat database jika belum ada
+def create_database(connection):
+    cursor = connection.cursor() # Gunakan cursor() untuk menjalankan perintah SQL.
+    cursor.execute("SHOW DATABASES") #Query ini digunakan untuk mendapatkan daftar semua database yang ada di server MySQL.
+    database = [db[0] for db in cursor.fetchall()] # Ambil daftar nama database
+
+    if "recipefinder" in database:
+        print("Database 'recipefinder' Sudah ada.")
+    # jika recipe_finder ada dalam daftar database ("recipe_finder" in databases), maka cetak pesan bahwa database sudah ada.
+    else:
+        cursor.execute("CREATE DATABASE recipefinder")
+        print("Database 'recipefinder' berhasil dibuat.")
+    # jika tidak ada, maka buat database menggunakan query CREATE DATABASE.
+
+    # Set database aktif
+    connection.database = "recipefinder" # Setelah memastikan database sudah ada, atur database aktif untuk koneksi dengan connection.database = "recipe_finder".
+    return connection
+# Tujuan: Membuat database bernama recipe_finder jika belum ada.
+# Parameter: connection adalah objek koneksi ke MySQL.
+
 if __name__ == "__main__": # Tujuan: Memastikan script hanya dijalankan sebagai file utama.
     conn = connect_to_database()
+    conn = create_database(conn) # Panggil fungsi create_database untuk mempersiapkan database.
     print("Terhubung ke database")
     
